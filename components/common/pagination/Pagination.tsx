967dce8fb7db6b66ad46898388372bbe5a583f82
suld2495 <suld7777@gmail.com>
2022-10-29 11:54:30 +0900

    [add] pagination 컴포넌트 임시 생성

---
+++ components/common/pagination/Pagination.tsx
@@ -0,0 +1,10 @@
+import { CommonProps } from "../type";
+
+type PaginationProps = CommonProps & {
+  page: number;
+  total: number;
+}
+
+export default function Pagination ({}: PaginationProps) {
+  
+}
