7247df03b6731a252261ff8ae7b646b6bd65f4c0
suld2495 <suld7777@gmail.com>
2022-10-29 11:54:08 +0900

    [change] 테이블 리팩토링

--- components/common/Table/Table.tsx
+++ components/common/Table/Table.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { composeClasses } from '../../../utils/composeClasses';
+import { CommonProps } from '../type';
 import styled from './table.module.scss';
 
 
 interface Column<RowData extends object> {
   title: string;
   field: keyof RowData | string;
+  align?: string;
 }
 
 interface RowData {
@@ -49,14 +52,16 @@ const TableBody = ({ columns, data }: { columns: Column<RowData>[], data: RowDat
   )
 };
 
-interface TableProps<RowData extends object> {
+type TableProps<RowData extends object> = CommonProps & {
   data: RowData[];
-  columns: Column<RowData>[]
+  columns: Column<RowData>[];
 }
 
-export default function Table({ data, columns }: TableProps<RowData>) {
+export default function Table({ data, columns, className = '', unStyled = false }: TableProps<RowData>) {
+  const tableClasses = composeClasses(className, unStyled ? '' : 'table');
+
   return (
-    <table className={styled.table}>
+    <table className={styled[tableClasses]}>
       <TableHead columns={columns} /> 
       <TableBody columns={columns} data={data} />
     </table>
