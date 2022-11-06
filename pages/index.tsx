7022c3cbf23604fbb72696a86bc0569877d521cb
suld2495 <suld7777@gmail.com>
2022-11-06 19:02:57 +0900

    [add] 임시 관리자 메인 페이지 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,61 @@
+import Title from 'components/admin/common/title/Title';
+import Button from 'components/common/button/Button';
+import Select from 'components/common/select/Select';
+import Table from 'components/common/table/Table';
 import type { NextPage } from 'next'
-import styles from './index.module.scss';
+import styled from './index.module.scss';
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.div}></div>
+    <div className={styled.div}>
+      <Title title='기업회원목록'/>
+      <div className={styled.selectBoxes}>
+        <Select name='1' defaultValue='전체' />
+        <Select name=''  defaultValue='분야' />
+        <Select name=''  defaultValue='기업명' />
+        <Select name=''  defaultValue='설립일' />
+      </div>
+      <Table
+        className={styled.table}
+        data={[]}
+        columns={[
+          {
+            title: 'no',
+            field: 'no'
+          }, {
+            title: '기업규모',
+            field: 'no1'
+          }, {
+            title: '기업명',
+            field: 'no2'
+          }, {
+            title: '근로자수',
+            field: 'no3'
+          }, {
+            title: '매출액',
+            field: 'no4'
+          }, {
+            title: '주요사업분야',
+            field: 'no5'
+          }, {
+            title: '산업구분',
+            field: 'no6'
+          }, {
+            title: '소재지',
+            field: 'no7'
+          }, {
+            title: '설립일',
+            field: 'no8'
+          }, {
+            title: '홈페이지',
+            field: 'no9'
+          }
+        ]}
+      />
+      <div className={styled.buttons}>
+        <Button>선택삭제</Button>
+      </div>
+    </div>
   )
 }
 
