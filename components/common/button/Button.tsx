25f8e9edbe2ecd7e4fb89d1d2d533fb664f2eca1
suld2495 <suld7777@gmail.com>
2022-12-05 22:25:17 +0900

    [change] 컴포넌트의 사이즈 변경 프로퍼티 추가

--- components/common/button/Button.tsx
+++ components/common/button/Button.tsx
@@ -7,19 +7,29 @@ import styled from './button.module.scss';
 type ButtonProps = CommonProps & {
   children?: React.ReactNode;
   href?: string;
+  width?: string;
+  height?: string;
 }
 
-export default function Button({ children, href, unStyled = false, className = '' }: ButtonProps) {
+export default function Button({ 
+  children, 
+  href, 
+  unStyled = false, 
+  className = '', 
+  width = '200px', 
+  height = '30px'
+}: ButtonProps) {
   const selectClasses = composeClasses(className, unStyled ? '' : styled.button);
+  const style = React.useMemo(() => ({ width, height }), [width, height]);
   
   return (
     <>
       {href ? (
         <Link href={href}>
-          <a className={selectClasses}>{children}</a>
+          <a className={selectClasses} style={style}>{children}</a>
         </Link>
       ) : (
-        <button className={selectClasses}>{children}</button>
+        <button className={selectClasses} style={style}>{children}</button>
       )}
     </>
   )
--- components/common/input/Input.tsx
+++ components/common/input/Input.tsx
@@ -4,32 +4,41 @@ import { CommonProps } from '../type';
 import styled from './input.module.scss';
 
 type InputProps = CommonProps & {
+  width?: string;
+  height?: string;
   name?: string;
   placeholder?: string;
   value?: string | number;
+  type?: 'text' | 'password';
   onChange?: React.ChangeEventHandler;
 }
 
 export default function Input({
+  width,
+  height,
   name,
   placeholder,
   value,
   className = '',
   unStyled,
-  onChange
+  onChange,
+  type = 'text'
 }: InputProps) {
   const [focus, setFocus] = React.useState(false);
   const inputClasses = composeClasses(className, unStyled ? '' : styled.input, focus ? styled.focus : '');
-  const handleFocus = React.useCallback(() => {
-    setFocus(true);
-  }, []);
-  const handleBlur = React.useCallback(() => {
-    setFocus(false);
-  }, []);
+  const style = React.useMemo(() => ({ width, height }), [width, height]);
+
   return (
-    <div className={inputClasses}>
+    <div className={inputClasses} style={style}>
       {value ? '' : <div className={styled.placeholder}>{placeholder}</div>}
-      <input value={value} name={name} onChange={onChange} onFocus={handleFocus} onBlur={handleBlur} />
+      <input 
+        value={value} 
+        name={name} 
+        type={type} 
+        onChange={onChange} 
+        onFocus={() => setFocus(true)} 
+        onBlur={() => setFocus(false)}
+      />
     </div>
   )
 }
